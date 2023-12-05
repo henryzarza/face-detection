ef064b0832d80c135da17a415720206fb47d40e9
Henry Zarza <henryacz7@gmail.com>
2023-12-05 15:30:46 +0200

    Starter files

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
