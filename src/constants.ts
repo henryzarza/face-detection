8875a2c5b33fe7550a2697fc7a530d1f071e6f7b
Henry Zarza <henryacz7@gmail.com>
2023-12-13 13:27:21 +0200

    Mask images

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,57 @@
+import mask1 from '@/masks/mask-1.png'
+import mask2 from '@/masks/mask-7.png'
+import mask3 from '@/masks/mask-3.png'
+import mask4 from '@/masks/mask-4.png'
+import mask5 from '@/masks/mask-5.png'
+import mask6 from '@/masks/mask-6.png'
+import mask7 from '@/masks/mask-2.png'
+import mask8 from '@/masks/mask-13.png'
+import mask9 from '@/masks/mask-10.png'
+import mask10 from '@/masks/mask-8.png'
+import mask11 from '@/masks/mask-9.png'
+import mask12 from '@/masks/mask-14.png'
+
 export const VIDEO_FACE_DIMENSIONS = {
   width: 450,
   height: 300
 }
+
+export const MASKS = [
+  {
+    src: mask1
+  },
+  {
+    src: mask2
+  },
+  {
+    src: mask3
+  },
+  {
+    src: mask4
+  },
+  {
+    src: mask5
+  },
+  {
+    src: mask6
+  },
+  {
+    src: mask7
+  },
+  {
+    src: mask8
+  },
+  {
+    src: mask9
+  },
+  {
+    src: mask10,
+    topAdjustment: 44
+  },
+  {
+    src: mask11
+  },
+  {
+    src: mask12
+  }
+]
